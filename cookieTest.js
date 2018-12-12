f39f0060b8bef7adf788839f57127dcc08364f69
alexniko01 <nikonenkoaleksandra@gmail.com>
2018-12-12 23:21:40 +0200

    vv

--- cookieTest.js
+++ cookieTest.js
@@ -1,105 +1,129 @@
 let homeworkContainer = document.querySelector('#homework-container');
 let heading = ['name', 'val', 'delete'];
 
-function sortData(arr) {
-    let result = {};
+class Helper {
+    sortData(arr) {
+        let result = {};
 
-    if (arr) {
-        result = arr.reduce((prev, cur) => {
-            const [name, val] = cur.split('=');
+        if (arr) {
+            result = arr.reduce((prev, cur) => {
+                const [name, val] = cur.split('=');
 
-            prev[name] = val;
-            return prev;
+                prev[name] = val;
+                return prev;
 
-        }, {})
+            }, {})
+        }
+
+        return result;
     }
 
-    return result;
 }
 
-function parseCookie() {
-    let cookieArr = document.cookie.split('; ');
+let helper = new Helper();
 
-    return sortData(cookieArr);
-}
+class Cookie {
+    constructor(helper, data) {
 
-function generateTable(hadings, cookies) {
+    }
 
-    let table = document.createElement('table');
-    let tableHeader = document.createElement('tr');
-    let title = document.createElement('caption');
+    parseCookie() {
+        let cookieArr = document.cookie.split('; ');
 
-    title.innerText = 'ALL COOKIEs';
-    table.appendChild(title);
+        return helper.sortData(cookieArr);
 
-    hadings.forEach((el) => {
-        let colName = document.createElement('th');
+    }
 
-        colName.innerText = el;
-        tableHeader.appendChild(colName);
-    });
-    table.appendChild(tableHeader);
-
-    if (Object.getOwnPropertyNames(cookies).length !== 0) {
-        for (let prop in cookies) {
-            if (!cookies.hasOwnProperty(prop) || !cookies[prop]) {
-                continue;
-            }
-            let row = document.createElement('tr');
-            let colName = document.createElement('td');
-            let colVal = document.createElement('td');
-            let btn = document.createElement('button');
-
-            btn.setAttribute('class', 'rmCookie');
-            btn.innerText = 'remove this cookie';
-
-            colName.innerText = prop;
-            colVal.innerText = cookies[prop];
-
-            row.appendChild(colName);
-            row.appendChild(colVal);
-            row.appendChild(btn);
-
-            table.appendChild(row);
+    addCookie(data) {
+        if (Object.getOwnPropertyNames(data).length !== 0) {
+            document.cookie = (data.name + '=' + data.val).toString();
         }
     }
-
-    table.setAttribute('border', '1px');
-    homeworkContainer.appendChild(table);
 }
 
-function createForm() {
-    let form = document.createElement('form');
-    let name = document.createElement('input');
-    let val = document.createElement('input');
-    let add = document.createElement('input');
-    let title = document.createElement('h1');
+let cookie = new Cookie();
 
-    title.innerText = 'ADD COOKIE:';
-    name.setAttribute('name', 'name');
-    name.setAttribute('placeholder', 'Name');
-    val.setAttribute('name', 'val');
-    val.setAttribute('placeholder', 'Value');
-    add.setAttribute('type', 'submit');
-    add.setAttribute('value', 'add cookie');
 
-    form.setAttribute('style', 'padding-bottom:200px');
-    form.setAttribute('id', 'cookieForm');
-    form.appendChild(title);
-    form.appendChild(name);
-    form.appendChild(val);
-    form.appendChild(add);
-    homeworkContainer.appendChild(form);
-}
-
-function addCookie(data) {
-    if (Object.getOwnPropertyNames(data).length !== 0) {
-        document.cookie = (data.name + '=' + data.val).toString();
+class Markup {
+    constructor() {
+        console.log(123);
     }
+
+    createForm() {
+        let form = document.createElement('form');
+        let name = document.createElement('input');
+        let val = document.createElement('input');
+        let add = document.createElement('input');
+        let title = document.createElement('h1');
+
+        title.innerText = 'ADD COOKIE:';
+        name.setAttribute('name', 'name');
+        name.setAttribute('placeholder', 'Name');
+        val.setAttribute('name', 'val');
+        val.setAttribute('placeholder', 'Value');
+        add.setAttribute('type', 'submit');
+        add.setAttribute('value', 'add cookie');
+
+        form.setAttribute('style', 'padding-bottom:200px');
+        form.setAttribute('id', 'cookieForm');
+        form.appendChild(title);
+        form.appendChild(name);
+        form.appendChild(val);
+        form.appendChild(add);
+        homeworkContainer.appendChild(form);
+    }
+
+    generateTable(hadings, cookies) {
+
+        let table = document.createElement('table');
+        let tableHeader = document.createElement('tr');
+        let title = document.createElement('caption');
+
+        title.innerText = 'ALL COOKIEs';
+        table.appendChild(title);
+
+        hadings.forEach((el) => {
+            let colName = document.createElement('th');
+
+            colName.innerText = el;
+            tableHeader.appendChild(colName);
+        });
+        table.appendChild(tableHeader);
+
+        if (Object.getOwnPropertyNames(cookies).length !== 0) {
+            for (let prop in cookies) {
+                if (!cookies.hasOwnProperty(prop) || !cookies[prop]) {
+                    continue;
+                }
+                let row = document.createElement('tr');
+                let colName = document.createElement('td');
+                let colVal = document.createElement('td');
+                let btn = document.createElement('button');
+
+                btn.setAttribute('class', 'rmCookie');
+                btn.innerText = 'remove this cookie';
+
+                colName.innerText = prop;
+                colVal.innerText = cookies[prop];
+
+                row.appendChild(colName);
+                row.appendChild(colVal);
+                row.appendChild(btn);
+
+                table.appendChild(row);
+            }
+        }
+
+        table.setAttribute('border', '1px');
+        homeworkContainer.appendChild(table);
+    }
+
 }
 
-createForm();
-generateTable(heading, parseCookie());
+let markup = new Markup();
+
+markup.createForm();
+markup.generateTable(heading, cookie.parseCookie());
 
 let form = document.getElementById('cookieForm');
 
@@ -108,20 +132,18 @@ form.addEventListener('submit', (e) => {
 
     let data = {};
     let formData = Object.values(form);
-    let name = '';
-    let val = '';
+
 
     formData.forEach((el) => {
         if (el.name === 'name') {
-            name = el.value;
+            data.name = el.value;
         }
         if (el.name === 'val') {
-            val = el.value;
+            data.val = el.value;
         }
     });
-    data[name] = val;
 
     console.log(data);
-    // addCookie(data)
+    cookie.addCookie(data)
 });
 
