2dac728c676d78b26c4e2d6483a5857db1481648
Oleksandra <o.semenchuk@devcaz.com>
2018-12-13 14:55:56 +0200

    xxxx

--- cookieTest.js
+++ cookieTest.js
@@ -23,10 +23,6 @@ class Helper {
 let helper = new Helper();
 
 class Cookie {
-    constructor(helper, data) {
-
-    }
-
     parseCookie() {
         let cookieArr = document.cookie.split('; ');
 
@@ -39,16 +35,23 @@ class Cookie {
             document.cookie = (data.name + '=' + data.val).toString();
         }
     }
+
+    filterCookie(existCookie, filterVal) {
+        for (let prop in existCookie) {
+            if ((prop.toLowerCase()).indexOf(filterVal.toLowerCase()) === -1
+                && (existCookie[prop].toLowerCase()).indexOf(filterVal.toLowerCase()) === -1) {
+                delete existCookie[prop];
+            }
+
+        }
+        return existCookie;
+
+    }
 }
 
 let cookie = new Cookie();
 
-
 class Markup {
-    constructor() {
-        console.log(123);
-    }
-
     createForm() {
         let form = document.createElement('form');
         let name = document.createElement('input');
@@ -58,13 +61,14 @@ class Markup {
 
         title.innerText = 'ADD COOKIE:';
         name.setAttribute('name', 'name');
+        name.setAttribute('type', 'text');
         name.setAttribute('placeholder', 'Name');
         val.setAttribute('name', 'val');
+        val.setAttribute('type', 'text');
         val.setAttribute('placeholder', 'Value');
         add.setAttribute('type', 'submit');
         add.setAttribute('value', 'add cookie');
 
-        form.setAttribute('style', 'padding-bottom:200px');
         form.setAttribute('id', 'cookieForm');
         form.appendChild(title);
         form.appendChild(name);
@@ -76,6 +80,8 @@ class Markup {
     generateTable(hadings, cookies) {
 
         let table = document.createElement('table');
+
+        table.setAttribute('id', 'cookieTable');
         let tableHeader = document.createElement('tr');
         let title = document.createElement('caption');
 
@@ -118,21 +124,31 @@ class Markup {
         homeworkContainer.appendChild(table);
     }
 
+    createFilterField() {
+        let filterField = document.createElement('input');
+
+        filterField.setAttribute('type', 'text');
+        filterField.setAttribute('id', 'filterCookie');
+        filterField.setAttribute('name', 'filterCookie');
+        filterField.setAttribute('placeholder', 'Filter');
+        filterField.setAttribute('style', 'margin-bottom:100px;margin-top:100px');
+
+        homeworkContainer.appendChild(filterField);
+    }
+
 }
 
 let markup = new Markup();
 
 markup.createForm();
+markup.createFilterField();
 markup.generateTable(heading, cookie.parseCookie());
-
 let form = document.getElementById('cookieForm');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener('submit', () => {
 
     let data = {};
     let formData = Object.values(form);
-
 
     formData.forEach((el) => {
         if (el.name === 'name') {
@@ -143,7 +159,20 @@ form.addEventListener('submit', (e) => {
         }
     });
 
-    console.log(data);
     cookie.addCookie(data)
 });
 
+let filter = document.getElementById('filterCookie');
+
+filter.addEventListener('keyup', () => {
+    let cookieObj = cookie.parseCookie();
+    let filteredCookie = cookie.filterCookie(cookieObj, filter.value);
+
+    let table = document.getElementById('cookieTable');
+
+    if (table) {
+        homeworkContainer.removeChild(table);
+    }
+
+    markup.generateTable(heading, filteredCookie);
+});
