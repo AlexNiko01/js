1d8b637de2c21b2fd27a53e3f154f1e0d2f08a06
alexniko01 <nikonenkoaleksandra@gmail.com>
2018-12-22 01:28:55 +0200

    refactor

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function auth() {
 
 function callAPI(method, params) {
     return new Promise((resolve, reject) => {
+        params.version = '5.92';
         VK.api(method, params, (data) => {
             if (data.error) {
                 reject(data.error);
@@ -26,17 +27,37 @@ function callAPI(method, params) {
     })
 }
 
-auth()
-    .then(() => {
-        return callAPI('users.get', {
-            fields: 'photo_100',
-            version: '5.92'
-        })
-    })
-    .then((response) => {
-        console.log(response);
-    });
+// auth()
+//     .then(() => {
+//         return callAPI('users.get', {
+//             name_case: 'gen',
+//             version: '5.92'
+//         })
+//     })
+//     .then((response) => {
+//         const headerInfo = document.querySelector('#headerInfo');
+//         headerInfo.textContent = `friends on page ${response[0].first_name} ${response[0].last_name}`;
+//         console.log(response);
+//     });
+(async () => {
+    await auth();
+    const [me] = await callAPI('users.get', {
+        name_case: 'gen',
 
+    });
+    const headerInfo = document.querySelector('#headerInfo');
+    headerInfo.textContent = `friends on page ${me.first_name} ${me.last_name}`;
+    const friends = await callAPI('friends.get', {
+        fields: 'city,country,photo_100'
+    });
+    console.log(friends);
+    const template = document.querySelector('#userTemplate').textContent;
+    console.log(Handlebars);
+    const templateCompiled = Handlebars.compile(template);
+    const results = document.getElementById('results');
+    console.log(results.innerHTML = templateCompiled({friends:friends}));
+
+})();
 
 
 
